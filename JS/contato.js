5cb3c1cccb00a95fb87730b79eb17ea214fa3d35
JosineudoA <96135176+JosineudoA@users.noreply.github.com>
2022-09-27 23:05:39 -0300

    função simples JS

---
+++ JS/contato.js
@@ -0,0 +1,7 @@
+function contato() {
+    alert("Contato da empresa: 8379-2372");
+    alert("Contato da empresa: cnn@gmail.com");
+}
+
+var btTelefone = document.getElementById("btTelefone");
+btTelefone.addEventListener("click", contato);
